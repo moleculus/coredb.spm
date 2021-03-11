3cad0cebb2cda272259d9b5740c7396c399fb325
Zakharov Viatcheslav <vz@moleculus.ru>
2021-03-11 16:15:35 +0100

    Fixes

--- Sources/CoreDB/CacheInitializable.swift
+++ Sources/CoreDB/CacheInitializable.swift
@@ -9,26 +9,26 @@ public protocol CacheInitializable: Identifiable {
 }
 
 public extension CacheInitializable {
-    static func cached(id: DBObject.ID) throws -> Self {
-        let cached = try DBCacheLoader<DBObject>().with(id: id)
+    static func cached(id: DBObject.ID, in realm: Realm = .default) throws -> Self {
+        let cached = try DBCacheLoader<DBObject>(in: realm).with(id: id)
         return try Self(cached: cached)
     }
     
-    static func cached(matching predicate: NSPredicate) throws -> Self {
-        let cached = try DBCacheLoader<DBObject>().with(predicate: predicate)
+    static func cached(matching predicate: NSPredicate, in realm: Realm = .default) throws -> Self {
+        let cached = try DBCacheLoader<DBObject>(in: realm).with(predicate: predicate)
         return try Self(cached: cached)
     }
     
-    static func allCached(matching predicate: NSPredicate? = nil) -> [Self] {
-        let cached = DBCacheLoader<DBObject>().with(predicate: predicate)
+    static func allCached(matching predicate: NSPredicate? = nil, in realm: Realm = .default) -> [Self] {
+        let cached = DBCacheLoader<DBObject>(in: realm).with(predicate: predicate)
         return cached.compactMap { try? Self(cached: $0) }
     }
     
-    static func removeAll() {
-        DBCacheEditor<DBObject>().remove(matching: nil)
+    static func removeAll(in realm: Realm = .default) {
+        DBCacheEditor<DBObject>(in: realm).remove(matching: nil)
     }
     
-    static func remove(matching predicate: NSPredicate) {
-        DBCacheEditor<DBObject>().remove(matching: predicate)
+    static func remove(matching predicate: NSPredicate, in realm: Realm = .default) {
+        DBCacheEditor<DBObject>(in: realm).remove(matching: predicate)
     }
 }
